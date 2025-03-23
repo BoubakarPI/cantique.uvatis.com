f57188897801b98537b237c6c7af32c388eb6b65
BoubakarPI <agenceweb1919@gmail.com>
2025-03-23 06:47:39 +0000

    feat: install UnoCSS presetIcons

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig, presetWind } from 'unocss'
+import { defineConfig, presetIcons, presetWind } from 'unocss'
 
 export default defineConfig({
-  presets: [presetWind()],
+  presets: [
+    presetWind(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+  ],
 })

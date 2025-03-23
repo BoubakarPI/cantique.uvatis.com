64dca87783f09d86abc4ea23d72c014a49ee69f6
BoubakarPI <agenceweb1919@gmail.com>
2025-03-23 06:48:13 +0000

    feat: create reusable ButtonBase component

---
+++ inertia/components/auth/ButtonBase.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  isLoading: boolean
+}>()
+</script>
+
+<template>
+  <button
+    type="submit"
+    class="w-full bg-primary hover:bg-primary-dark text-white font-medium py-2 px-4 rounded-md transition-colors duration-200 ease-in-out"
+    :class="{ 'opacity-70 cursor-not-allowed': isLoading }"
+    :disabled="isLoading"
+  >
+    <span v-if="isLoading" class="flex items-center justify-center">
+      <div class="i-svg-spinners:bars-scale w-6 h-6"> </div>
+
+    </span>
+    <span v-else>{{  label }}</span>
+  </button>
+</template>
